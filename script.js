6dc01c61624eb0bdb2beecce7861ea5cb1ef64c0
Nadaakaram <nadakkaram@gmail.com>
2024-06-12 04:07:20 +0300

    add style

---
+++ script.js
@@ -0,0 +1,12 @@
+// let frname = document.getElementsById('frname');
+let lname = document.getElementsById('lname');
+let email = document.getElementsById('email');
+let messages = document.getElementsById('messages');
+let submit = document.getElementsById('submit');
+// let form = document.getElementsById('form').value;
+console.log(lname);
+
+
+// submit.onClick = function(){
+//     console.log(frname.value);
+// }
